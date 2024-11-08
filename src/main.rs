556b9880a913a91679551c35775c1b5f4cd0e3d5
Preedee Ponchevin <preedee.digital@gmail.com>
2024-11-08 20:04:08 +0700

    Add SMTP email sending functionality
    This commit integrates SMTP email sending using the lettre crate. It retrieves SMTP credentials and server details from environment variables, constructs the email message, and sends it using the specified SMTP server. Appropriate logging has been added for success and error scenarios.

--- src/main.rs
+++ src/main.rs
@@ -12,8 +12,9 @@ mod models;
 mod utils;
 
 use clap::{Parser, Subcommand, ValueEnum};
-use lettre::Message;
+use lettre::{Message, SmtpTransport, Transport};
 use lettre::message::header::ContentType;
+use lettre::transport::smtp::authentication::Credentials;
 
 #[derive(Debug,Clone,ValueEnum)]
 enum ACSProtocol {
@@ -31,14 +32,37 @@ struct Cli {
     protocol: ACSProtocol,
 }
 async fn send_email_with_smtp(){
+
+    let sender = env::var("SENDER").unwrap();
+    let reply_email_to = env::var("REPLY_EMAIL").unwrap();
+
+    let smtp_server = env::var("SMTP_SERVER").unwrap();
+    let smtp_user = env::var("SMTP_USER").unwrap();
+    let smtp_password = env::var("SMTP_PASSWORD").unwrap();
+
     let email = Message::builder()
-        .from("NoBody <nobody@domain.tld>".parse().unwrap())
-        .reply_to("Yuin <yuin@domain.tld>".parse().unwrap())
-        .to("Hei <hei@domain.tld>".parse().unwrap())
+        .from(sender.parse().unwrap())
+        //.reply_to(reply_email_to.parse().unwrap())
+        .to(reply_email_to.parse().unwrap())
         .subject("Happy new year")
         .header(ContentType::TEXT_PLAIN)
         .body(String::from("Be happy!"))
         .unwrap();
+
+
+    let creds = Credentials::new(smtp_user, smtp_password);
+
+    // Open a remote connection to gmail
+    let mailer = SmtpTransport::relay(smtp_server.as_str())
+        .unwrap()
+        .credentials(creds)
+        .build();
+
+    // Send the email
+    match mailer.send(&email) {
+        Ok(_) => info!("Email sent successfully!"),
+        Err(e) => error!("Could not send email: {e:?}"),
+    }
 }
 async fn send_email_with_api(){
     let connection_str = env::var("CONNECTION_STR").unwrap();
