f0e5e45efdd50ebcef93e6ba5a7a0e417b145ec3
Preedee Ponchevin <preedee.digital@gmail.com>
2024-11-12 19:34:26 +0700

    Add license information to acs_email.rs
    Include MIT license details and project description to ensure compliance and clarity regarding the terms of use for the Azure Communication Services Email Client Library. This addition helps in maintaining transparency and adhering to open-source project requirements.

--- src/acs_email.rs
+++ src/acs_email.rs
@@ -1,3 +1,6 @@
+// License: MIT
+// This file is part of the Azure Communication Services Email Client Library, an open-source project.
+// This source code is licensed under the MIT license found in the LICENSE file in the root directory of this source tree.
 use crate::models::{
     EmailSendStatusType, ErrorDetail, ErrorResponse, SentEmail, SentEmailResponse,
 };
